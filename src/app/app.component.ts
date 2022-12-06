5a8d86fe0ae39e20665cb1087405eaea3dc060d0
Alik Dev <moisesalik01@gmail.com>
2022-12-06 01:26:50 -0500

    tailwind add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <section class="flex">
+
+      <article class="dark:bg-slate-800 dark:text-slate-50 h-screen p-3" id="sidebar">
+        <h3 class="text-center text-4xl font-bold">{{title}}</h3>
+        <hr class="my-3">
+        <nav class="flex flex-col bg-slate-100 text-black rounded-sm">
+          <a href="#" class="p-2 border-b border-slate-800">
+            dashboard
+          </a>
+        </nav>
+      </article>
+
+      <article class="basis-full">
+
+        <div class="px-6 pt-6">
+          <h3 class="text-lg">Buscar:</h3>
+          <input type="text" placeholder="Buscar.." class="outline-none pl-4 py-2 w-full border border-slate-600 rounded-sm">
+        </div>
+
+        <div class="p-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quia incidunt neque architecto velit. Error sunt quidem eum saepe quasi hic impedit, ullam beatae cumque.
+          </p>
+        </div>
+
+      </article>
+
+    </section>`,
 })
 export class AppComponent {
-  title = 'gifsapp';
+
+  public title: string = 'gifsapp';
 }
